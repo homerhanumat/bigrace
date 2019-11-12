36103f465854ababacd34817bc9e322fbc8ca6b7
Homer White <homerhanumat@gmail.com>
2019-11-12 16:13:19 -0500

    initial commit

---
+++ R/data.R
@@ -0,0 +1,81 @@
+#' Marathon Runners
+#'
+#' @description Runners in a hypothetical marathon.
+#'
+#' @name Racers
+#' @docType data
+#' @format A data frame with 200 observations on the following 6 variables.
+#' \describe{
+#' \item{id}{ID number of the runner}
+#' \item{first_name}{Runner's first name}
+#' \item{last_name}{Runner's last name}
+#' \item{sex}{Sex of the runner:  "F" or "M"}
+#' \item{dob}{Date of birth of the runner}
+#' \item{team}{ID of the team (if any) that the runner belongs to}
+#' }
+#'
+#' @source  Hypothetical data.
+#' @keywords datasets
+#'
+NA
+#'
+#' Marathon Results
+#'
+#' @description Results of a hypothetical marathon.
+#'
+#' @name Results
+#' @docType data
+#' @format A data frame with 200 observations on the following 3 variables.
+#' \describe{
+#' \item{id}{ID number of the runner}
+#' \item{time}{Runner's finishing time, as hours, minutes and seconds}
+#' \item{seconds}{Runner's finishing time in seconds}
+#' }
+#'
+#' @source  Hypothetical data.
+#' @keywords datasets
+#'
+NA
+#'
+#' Marathon Teams
+#'
+#' @description Teams in a hypothetical marathon.
+#'
+#' @name teams
+#' @docType data
+#' @format A data frame with 15 observations on the following 2 variables.
+#' \describe{
+#' \item{teamID}{ID of the team}
+#' \item{team_name}{full name of the team}
+#' }
+#'
+#' @source  Hypothetical data.
+#' @keywords datasets
+#'
+NA
+#'
+#' Marathon Runners
+#'
+#' @description A table produced by joining some other tables
+#' in this package.
+#'
+#' @name Marathon
+#' @docType data
+#' @format A data frame with 200 observations on the following 9 variables.
+#' \describe{
+#' \item{id}{ID number of the runner}
+#' \item{first_name}{Runner's first name}
+#' \item{last_name}{Runner's last name}
+#' \item{sex}{Sex of the runner:  "F" or "M"}
+#' \item{dob}{Date of birth of the runner}
+#' \item{team}{ID of the team (if any) that the runner belongs to}
+#' \item{time}{Runner's finishing time, as hours, minutes and seconds}
+#' \item{seconds}{Runner's finishing time in seconds}
+#' \item{team_name}{full name of the team (if any) the runner belongs to}
+#' }
+#'
+#' @source  Hypothetical data.
+#' @keywords datasets
+#'
+NA
+
